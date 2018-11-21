57a6b1e97c6fdcad3362c1f2489727bfd1acac57
Vankxr <jgc3silva@gmail.com>
2018-11-21 16:30:02 +0000

    Fix errors 2

--- mcp2210test.cpp
+++ mcp2210test.cpp
@@ -337,11 +337,11 @@ int main(int argc, char** argv) {
 
     MCP_CAN *mcp2515 = new MCP_CAN(handle);
 
-    mcp2515.begin();
+    mcp2515->begin();
 
     while(1)
     {
-        
+
     }
 
     /**
